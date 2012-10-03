1f45a45e8d006153f6ddb8747718cdfed58814db
Buddy Magsipoc <keikun17@gmail.com,buddy@aelogica.com>
2012-10-03 14:18:32 +0800

    added factory_crush

---
+++ factory_crush.rb
@@ -0,0 +1,38 @@
+# There's this girl
+FactoryGirl.define do
+  # that i really like.
+  factory :crush do
+    name "Christine Reyes"
+    face "9001"
+
+    # But she is a
+    high_maintenance
+    starlet
+
+    trait :high_maintenance  do
+      bag     "LV"
+      makeup  "MAC"
+    end
+
+    trait :starlet do
+      suitors "9001"
+    end
+
+  end
+end
+
+include DateHelper # I need dating tips.
+# So is there a way for someone like me to date someone like Christine Reyes?
+
+factoryGirl.modify do
+  # with paltry savings from my daily allowance?
+
+  factory :crush do
+    low_maintenance
+  end
+
+  trait :low_maintenance do
+    bag     "Fake Jansport"
+    makeup  nil
+  end
+end
